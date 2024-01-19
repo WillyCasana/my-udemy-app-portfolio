cc1ccd76f37b38e12e168d08f2a451ecca0b1d3e
Willy Roberto Casana Saavedra <pex0000443@prosegur.com>
2024-01-18 23:28:53 -0500

    added techs

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import NavigationBar from './screens/navigationBar/NavigationBar';
 import Particles from '@tsparticles/react'
 import AboutMe from './screens/aboutMe/AboutMe';
 
+import Technologies from './screens/technologies/Technologies';
+
 function App() {
   return (
     <div className="App">
       <NavigationBar />
 
       <AboutMe />
+      <Technologies />
     </div>
   );
 }
